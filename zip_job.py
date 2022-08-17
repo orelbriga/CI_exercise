8d01a24c476dc18997cacd891606c330ec041c33
Orel Briga <orelbriga@gmail.com>
2022-08-17 21:25:17 +0300

    new jenkins files

--- zip_job.py
+++ zip_job.py
@@ -6,27 +6,20 @@ print(verENV)
 
 
 def createZip(txtFile):
-    zipObj = ZipFile(f'{txtFile}_{verENV}.zip','w')
+    zipObj = ZipFile(f'{txtFile}_{verENV}.zip', 'w')
     zipObj.write(f'{txtFile}.txt')
     zipObj.close()
     return f'{txtFile}_{verENV}.zip'
 
-files = ['a','b','c','d']
+
+files = ['a', 'b', 'c', 'd']
 for file in files:
-    with open(f"{file}.txt","w") as f:
+    with open(f"{file}.txt", "w") as f:
         if not os.path.isfile(f"{file}.txt"):
             exit(print(f'py script aborted, failed to create {file}.txt'))
     zippedFile = createZip(file)
     os.remove(f'{file}.txt')
-    if not os.path.isfile((zippedFile)):
+    if not os.path.isfile(zippedFile):
         exit(print(f'py script aborted, failed to create {zippedFile}'))
 
 print('All files were created successfully.')
-
-
-
-
-
-
-
-
