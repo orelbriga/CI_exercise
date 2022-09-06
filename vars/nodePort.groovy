fd884a76e466992520dd8c632f490ff00fb48209
Orel Briga <orelbriga@gmail.com>
2022-09-06 23:12:12 +0300

    fixing nodePort var

--- vars/nodePort.groovy
+++ vars/nodePort.groovy
@@ -1,6 +1,15 @@
-def call() {
+public void nodePort(body) {
     sh(
             script: './kubectl get svc hello-world-svc-$BUILD_NUMBER -o=jsonpath=\'{.spec.ports[].nodePort}\' ',
             returnStdout: true
-    ).trim()
+    ).trim() {
+        body.call()
+    }
 }
+
+//def call() {
+//    sh(
+//            script: './kubectl get svc hello-world-svc-$BUILD_NUMBER -o=jsonpath=\'{.spec.ports[].nodePort}\' ',
+//            returnStdout: true
+//    ).trim()
+//}
--- vars/testCall.groovy
+++ vars/testCall.groovy
@@ -1,15 +1,3 @@
 def call() {
     echo "testing call method.."
 }
-
-
-
-public void agentTemplate(body) {
-    podTemplate(
-            containers: [
-                    containerTemplate(name: 'gradle', image: 'gradle:7.5.1-jdk11-jammy', resourceLimitMemory:'1024Mi', resourceRequestMemory:'512Mi', command: 'cat', ttyEnabled: true),
-                    containerTemplate(name: 'docker', image: 'docker', resourceLimitMemory:'1024Mi', resourceRequestMemory:'512Mi', command: 'cat', ttyEnabled: true)],
-            volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
-        body.call()
-    }
-}
