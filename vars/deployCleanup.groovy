6d9f639f87c6a9db314317cbce4bf7d30e60a969
Orel Briga <orelbriga@gmail.com>
2022-09-09 22:04:55 +0300

    adding deployCleanup.groovy

---
+++ vars/deployCleanup.groovy
@@ -0,0 +1,9 @@
+def terminateApp() {
+    sh './kubectl delete deployment,services -l app=hello-world-app-${BUILD_NUMBER}'
+}
+
+
+def deleteImage() {
+    sh '''docker image rmi -f orelbriga/hello-world-app:$BUILD_NUMBER'
+          docker image prune -f  --filter="dangling=true" '''
+}
