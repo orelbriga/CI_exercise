4ebfbe11ffc0284f1efa7c27b610fbf1e2b9bfbb
Orel Briga <orelbriga@gmail.com>
2022-09-09 13:04:53 +0300

    adding deployVars.groovy

--- vars/deployVars.groovy
+++ vars/deployVars.groovy
@@ -7,9 +7,20 @@ def appName() {
 }
 
 
-def CLUSTER_HOST_IP = sh(
+def podState = sh(
+        script: './kubectl get po | grep hello-world-app-$BUILD_NUMBER-* | \
+                                    awk \'{print $3}\'',
+        returnStdout: true
+).trim()
+
+
+def clusterHostIP = sh(
         script: './kubectl get pod -n kube-system $(./kubectl get po -n kube-system | grep dns \
                             | awk \'{print $1}\') -o=jsonpath=\'{.status.hostIP}\' ', returnStdout: true
 ).trim()
 
 
+def nodePort = sh(
+        script: './kubectl get svc hello-world-svc-$BUILD_NUMBER -o=jsonpath=\'{.spec.ports[].nodePort}\' ',
+        returnStdout: true
+).trim()
