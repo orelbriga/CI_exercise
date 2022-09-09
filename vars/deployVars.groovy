e6878f224aaef1fb7cdce8083ace3087cbd71694
Orel Briga <orelbriga@gmail.com>
2022-09-09 12:28:46 +0300

    adding deployVars.groovy

--- vars/deployVars.groovy
+++ vars/deployVars.groovy
@@ -1,13 +1,6 @@
-def call(body) {
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-
-    def CLUSTER_HOST_IP = sh(
-            script: './kubectl get pod -n kube-system $(./kubectl get po -n kube-system | grep dns \
-                                | awk \'{print $1}\') -o=jsonpath=\'{.status.hostIP}\' ', returnStdout: true
-    ).trim()
+def CLUSTER_HOST_IP = sh(
+        script: './kubectl get pod -n kube-system $(./kubectl get po -n kube-system | grep dns \
+                            | awk \'{print $1}\') -o=jsonpath=\'{.status.hostIP}\' ', returnStdout: true
+).trim()
 
 
-}
