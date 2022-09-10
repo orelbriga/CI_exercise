c8139aa7e85853a52d790573e90ddbcb9abf0a67
Orel Briga <orelbriga@gmail.com>
2022-09-10 22:08:18 +0300

    adding dockerImage.groovy

--- vars/deployVars.groovy
+++ vars/deployVars.groovy
@@ -57,3 +57,13 @@ def getRequest(Map config = [:]) {
     def RESPONSE = httpRequest "http://${IP}:${PORT}"
     println("Content: " + RESPONSE.content)
 }
+
+def build() {
+    withCredentials([[$class: 'UsernamePasswordMultiBinding',
+                      credentialsId: 'dockerhub',
+                      usernameVariable: 'DOCKER_HUB_USER',
+                      passwordVariable: 'DOCKER_HUB_PASSWORD']]) {
+        sh """ echo ${DOCKER_HUB_PASSWORD} | docker login --username ${DOCKER_HUB_USER} --password-stdin
+                       docker build -t ${env.REPOSITORY}:${env.BUILD_NUMBER} . """
+    }
+}
