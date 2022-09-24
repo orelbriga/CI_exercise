88d33bccc8b99f5c699a6502775ae45821124076
Orel Briga <orelbriga@gmail.com>
2022-09-24 18:31:34 +0300

    add script

--- resources/com/ci-task/python-zip-pipeline/onStartUp.sh
+++ resources/com/ci-task/python-zip-pipeline/onStartUp.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-echo OS Type: "$(cat /etc/redhat-release)" ; \
-test -f /tmp/zip_job.py && echo "zip_job.py exists on /tmp dir." || echo "zip_job.py does not exist on /tmp dir." ; \
+echo OS Type: "$(cat /etc/redhat-release)"
+test -f /tmp/zip_job.py && echo "zip_job.py exists on /tmp dir." || echo "zip_job.py does not exist on /tmp dir."
 cat
