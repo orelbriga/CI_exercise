e567ee78a9835a340070333df967e842a41396a8
Orel Briga <orelbriga@gmail.com>
2022-09-24 15:18:10 +0300

    set branch draft on pipeline

--- vars/cicdScript.groovy
+++ vars/cicdScript.groovy
@@ -10,7 +10,7 @@ def call () {
             try {
                 stage('Git checkout') {
                     container('gradle') {
-                        checkout([$class: 'GitSCM', branches: [[name: '*/master']], userRemoteConfigs: [[credentialsId: 'github-private',\
+                        checkout([$class: 'GitSCM', branches: [[name: '*/draft']], userRemoteConfigs: [[credentialsId: 'github-private',\
                                   url: 'https://github.com/orelbriga/hello-world.git']]])
                     }
                 }
