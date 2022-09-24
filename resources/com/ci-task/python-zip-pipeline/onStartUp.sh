53f6fb069c1e92dfecabe5df4704a36af2a80e50
Orel Briga <orelbriga@gmail.com>
2022-09-24 17:42:56 +0300

    optimzie centospy Dockerfile by adding exteral script

---
+++ resources/com/ci-task/python-zip-pipeline/onStartUp.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo OS Type: "$(cat /etc/redhat-release)" ; \
+test -f /tmp/zip_job.py && echo "zip_job.py exists on /tmp dir." || echo "zip_job.py does not exist on /tmp dir." ; \
+cat
