f7aa528fe6ccb0a3829adb3af2beff6a1dd1cb17
Orel Briga <orelbriga@gmail.com>
2022-09-26 04:01:09 +0300

    test build-cache

--- vars/cicdScript.groovy
+++ vars/cicdScript.groovy
@@ -20,9 +20,9 @@ def call () {
                             log.info "compiling code + running  tests: "
                             sh "chmod +x ./gradlew"
                             sh "mkdir -p /gradlePV/tmp-cache"
-                            sh "\'cp -r /gradlePV/tmp-cache/* build-cache"
+                            sh script: " \\cp -r /gradlePV/tmp-cache/* build-cache"
                             sh  "./gradlew --build-cache test "
-                            sh "\'cp -r build-cache/* /gradlePV/tmp-cache"
+                            sh "\\cp -r build-cache/* /gradlePV/tmp-cache"
                         }
                         catch (e) {
                             error("some of the tests have failed - $e ")
